1a1d3c5096697926911cc8bf34a7df4d8b9e7382
cpr-mab <Markus.Broxtermann@commonplacerobotics.de>
2026-06-18 13:55:31 +0200

    CI set up

--- CRI_Client/MainForm.cs
+++ CRI_Client/MainForm.cs
@@ -12,6 +12,12 @@ namespace CRI_Client
 {
     public partial class MainForm : Form
     {
+        // Version of the CRI Demo Client
+        // Also update the lifecycle info in sbomify.json!
+        public static readonly int SWVersionMajor = 3;
+        public static readonly int SWVersionMinor = 7;
+        public static readonly int SWVersionPatch = 1;
+        public static readonly string SWVersionString = string.Format("{0}.{1}.{2}", SWVersionMajor, SWVersionMinor, SWVersionPatch);
 
         // Create a logger for use in this class
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
